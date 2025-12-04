b855d797055de0c4a76f4e62aaa2779d13f82d40
Sebastian Cacchione <scacchione2008@gmail.com>
2025-12-04 00:33:06 -0500

    Add favicon and app icon links

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,6 +15,13 @@ export default function Document() {
           async
           data-softgen-monitoring="true"
         />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <body
         className={cn(
